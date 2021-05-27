aa94d556d34075145d668ea0b9c76ea3bb4dd00c
SmireG <60234283+SmireG@users.noreply.github.com>
2021-05-27 22:40:28 +0800

    update DockingTest

--- ProgramTracer/test/DockingTest.cpp
+++ ProgramTracer/test/DockingTest.cpp
@@ -1,12 +1,21 @@
 //
-// Created by Albert on 2021/5/27.
+// Created by gaosd on 2021/5/22.
 //
-#include <stdio.h>
-#include <stdlib.h>
+
+#include <unistd.h>
+#include <dirent.h>
 #include <iostream>
 
+using namespace std;
 int main() {
-    int *ptr = new int(10);
-    delete ptr;
+
+    while (1) {
+	sleep(3);
+	int*a = new int[10000000];
+	int*b = new int[10000000];
+        cout << "process " << getpid() << endl;
+        DIR *dir;
+        dir = opendir("/proc");
+    }
     return 0;
 }
